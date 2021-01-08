8c20f080f056e497be2b10ef3339119d3edec44a
puneeth-techie <puneethpgowda.techie@gmail.com>
2021-01-08 21:07:04 +0530

    Changed DB setting ENV variables.

--- startup/db.js
+++ startup/db.js
@@ -1,10 +1,20 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
+// const winston = require('winston');
+
+// module.exports = function() {
+//     mongoose.connect('mongodb://localhost/nodeAPIProject', { 
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true,
+//         useCreateIndex: true})
+//     .then(() => console.info('Connected to the MongoDB...!'));
+// }
+
 const winston = require('winston');
+const mongoose = require('mongoose');
+const config = require('config');
 
 module.exports = function() {
-    mongoose.connect('mongodb://localhost/nodeAPIProject', { 
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true})
-    .then(() => console.info('Connected to the MongoDB...!'));
+  const db = config.get('db');
+  mongoose.connect(db)
+    .then(() => winston.info(`Connected to ${db}...`));
 }
